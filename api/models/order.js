6bea7468d3833c0dc152d2611bf17a1f4ab37465
noor <khannoor151@gmaill.com>
2021-12-17 11:30:57 +0530

    ordersjs

--- api/models/order.js
+++ api/models/order.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 
 const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    product:{type: mongoose.Schema.Types.ObjectId,ref: 'Product'},
-    quantity: {type: Number, default: 1, required: true}
-});
+    product: {type: mongoose.Schema.Types.ObjectId, ref: 'Product'},
+    quantity: {type: Number, default: 1, require: ture}
+})
 
 module.exports = mongoose.model('order',orderSchema);
     
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,6 +5,27 @@ const mongoose = require('mongoose');
 const Order = ('../models/order');
 
 router.get('/',(req,res,next) =>{
+    const order = new Order({
+        _id: mongoose.Schema.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
+    });
+    order
+    .save()
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.status(201).json(result);
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        });
+    })
+
+    
+
     res.status(200).json({
         message:"orders were fetched"
     });
