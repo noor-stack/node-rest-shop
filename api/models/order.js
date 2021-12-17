0da2c2848c3f5f948f861fc1ff0feffd6840f8fe
noor <khannoor151@gmaill.com>
2021-12-17 09:30:29 +0530

    order

--- api/models/order.js
+++ api/models/order.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    product: { type: mongoose.Schema.Types.ObjectId, ref:'Product',required: true},
-    quantity: {type: Number, default:1, required: true}
+    product:{type: mongoose.Schema.Types.ObjectId,ref: 'Product'},
+    quantity: {type: Number, default: 1, required: true}
 });
 
-module.exports = mongoose.model('Order',productSchema);
+module.exports = mongoose.model('order',orderSchema);
+    
+    
